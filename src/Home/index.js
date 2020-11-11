3a60f95d27a91ab7695842e1fe7f4cb806bae5cd
TamHT <tamht3698@gmail.com>
2020-11-11 21:02:37 +0700

    create header component

---
+++ src/Home/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./Header";
+
+const Home = (props) => {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+};
+
+export default Home;
