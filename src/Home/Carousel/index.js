98c93e611519f5daf00ab2331d8cc8b93e5bcf3e
TamHT <tamht3698@gmail.com>
2021-01-04 22:50:23 +0700

    add carousel animation

--- src/Home/Carousel/index.js
+++ src/Home/Carousel/index.js
@@ -33,25 +33,9 @@ const Carousel = (props) => {
       activeSlide: activeSlide === 0 ? slides.length - 1 : activeSlide - 1,
     });
 
-  const smoothTransition = () => {
-    let _slides = [];
-
-    if (activeSlide === slides.length - 1)
-      _slides = [slides[slides.length - 2], lastSlide, firstSlide];
-    else if (activeSlide === 0) _slides = [lastSlide, firstSlide, secondSlide];
-    else _slides = slides.slice(activeSlide - 1, activeSlide + 2);
-
-    setState({
-      ...state,
-      _slides,
-      transition: 0,
-      translate: window.innerWidth,
-    });
-  };
-
   return (
     <Slider ref={sliderRef}>
-      <SliderContent width={window.innerWidth * _slides.length}>
+      <SliderContent width={window.innerWidth * _slides.length} translate={translate}>
         {_slides.map((_slide, i) => (
           <Slide width={window.innerWidth} key={_slide + i} content={_slide} />
         ))}
